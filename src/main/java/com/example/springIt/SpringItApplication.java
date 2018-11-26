ba9cbd554c61a07caa4e20b73d064698e49fdea8
Dave Engle <davengle@gmail.com>
2018-11-25 19:31:12 -0500

    Init commit for SpringBoot reddit clone

---
+++ src/main/java/com/example/springIt/SpringItApplication.java
@@ -0,0 +1,12 @@
+package com.example.springIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringItApplication.class, args);
+	}
+}
